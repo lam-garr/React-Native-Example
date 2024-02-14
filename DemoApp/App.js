9f27a97e6053785e4e88687e65f0e58b14646075
Garrett <lamgarr@outlook.com>
2024-02-13 20:07:05 -0800

    Update react-native imports

--- DemoApp/App.js
+++ DemoApp/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Pressable, Alert } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Pressable title="button"
+        onPress={() => {
+          Alert.alert("Alert Title", "Alert Message", [
+            {text:"Yes", onPress: () => console.log("yes")},
+            {text:"No", onPress: () => console.log("No")}
+          ])
+        }}
+      />
     </SafeAreaView>
   );
 }
