9f4b22bc2d840221dcdcb61b3fcbe3aece24c6d7
Garrett <lamgarr@outlook.com>
2024-02-26 19:14:17 -0800

    Add Signup Screen

---
+++ DemoApp/app/screens/SignupScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+function SignupScreen() {
+    return(
+        <SafeAreaView style={styles.background}>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.Create({
+    background: {
+        flex: 1,
+        backgroundColor: "white"
+    }
+})
+
+export default SignupScreen;
