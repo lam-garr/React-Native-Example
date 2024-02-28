fb0282fc1879447c3c953ec1b99b928fa65cd8ad
Garrett <lamgarr@outlook.com>
2024-02-27 18:17:12 -0800

    Add Stack Navigation

--- DemoApp/App.js
+++ DemoApp/App.js
@@ -1,9 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Pressable, Alert } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import AboutScreen from './app/screens/AboutScreen';
+import SupportScreen from './app/screens/SupportScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <WelcomeScreen/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={WelcomeScreen}/>
+        <Stack.Screen name="About" component={AboutScreen}/>
+        <Stack.Screen name="Support" component={SupportScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
