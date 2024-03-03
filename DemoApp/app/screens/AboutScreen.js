fa314092da430fbc901d2c125f17d076c69a45b1
Garrett <lamgarr@outlook.com>
2024-03-02 21:53:46 -0800

    Add handling to return data from About screen back to Home

--- DemoApp/app/screens/AboutScreen.js
+++ DemoApp/app/screens/AboutScreen.js
@@ -11,6 +11,10 @@ function AboutScreen({ route, navigation }) {
             <Button style={styles.button} title="Update" onPress={() => navigation.setParams({
                 name:"Updated User"
             })}/>
+            <Button title="Return with data" onPress={() => navigation.navigate(
+                "Home",
+                {result: "Data from About"}
+            )}/>
         </SafeAreaView>
     );
 }
--- DemoApp/app/screens/WelcomeScreen.js
+++ DemoApp/app/screens/WelcomeScreen.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View, Image, ScrollView, Button } from "react-native";
+import { ImageBackground, StyleSheet, View, Image, ScrollView, Button, Text } from "react-native";
 import { Stack, useRouter } from "expo-router";
 import { useNavigation } from "@react-navigation/native";
 
-function WelcomeScreen({ navigation }) {
+function WelcomeScreen({ navigation, route }) {
     const navigation = useNavigation();
     return(
         <ImageBackground style={styles.background}>
             <Image/>
             <View style={styles.welcomeButton}></View>
             <Button title="About" onPress={() => navigation.navigate("About", {name:"User"})}/>
+            <Text style={styles.paramsText}>{route.params?.result}</Text>
         </ImageBackground>
     );
 }
@@ -23,6 +24,9 @@ const styles = StyleSheet.Create({
         width: "95%",
         height: 70,
         backgrounColor: "pink"
+    },
+    paramsText: {
+        color: black
     }
 })
 
