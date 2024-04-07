d1c6d5b129ba309679418f145b7fff79df172b6b
Garrett <lamgarr@outlook.com>
2024-04-06 22:41:00 -0700

    Add navigation from Support screen back to Home

--- DemoApp/app/screens/SupportScreen.js
+++ DemoApp/app/screens/SupportScreen.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
 
-function SupportScreen({ route }) {
+function SupportScreen({ route, navigation }) {
 
     const { name } = route.params;
 
     return(
         <SafeAreaView style={styles.background}>
             <Text style={styles.text}>Hello {name}</Text>
+            <Button title="Return with data" onPress={() => navigation.navigate(
+                "Home",
+                {result: "Data from Support"}
+            )}/>
         </SafeAreaView>
     );
 }
