04f33898b7e966007ebe4b3b40eee349a702e7a8
Garrett <lamgarr@outlook.com>
2024-04-07 19:41:36 -0700

    Add handling to set params in Support screen

--- DemoApp/app/screens/SupportScreen.js
+++ DemoApp/app/screens/SupportScreen.js
@@ -8,6 +8,9 @@ function SupportScreen({ route, navigation }) {
     return(
         <SafeAreaView style={styles.background}>
             <Text style={styles.text}>Hello {name}</Text>
+            <Button style={styles.button} title="Update" onPress={() => navigation.setParams({
+                name:"Updated Support User"
+            })}/>
             <Button title="Return with data" onPress={() => navigation.navigate(
                 "Home",
                 {result: "Data from Support"}
