de355094bd079eb44c1629e58245529038ac68d4
Garrett <lamgarr@outlook.com>
2024-04-19 23:04:51 -0700

    Edit styling in Support screen

--- DemoApp/app/screens/SupportScreen.js
+++ DemoApp/app/screens/SupportScreen.js
@@ -25,7 +25,10 @@ const styles = StyleSheet.Create({
         backgroundColor: "white"
     },
     text:{
-        color: black
+        color: "black"
+    },
+    button:{
+        color: "black"
     }
 })
 
