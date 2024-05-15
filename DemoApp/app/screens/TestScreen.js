e8e01607ae89b2ebf2e7622df5d696be8691fcd5
Garrett <lamgarr@outlook.com>
2024-05-14 20:06:44 -0700

    Add Test screen

---
+++ DemoApp/app/screens/TestScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Button, ScrollView } from "react-native";
+
+function TestScreen() {
+    return(
+        <View style={{flex:1, backgroundColor: "plum", padding: 60}}>
+            <ScrollView>
+            </ScrollView>
+        </View>
+    );
+}
+
+export default TestScreen; 
