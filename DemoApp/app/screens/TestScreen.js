0e660813b75de07108212f4e27a6dc2705b8d4b1
Garrett <lamgarr@outlook.com>
2024-05-16 19:22:34 -0700

    Add Image to Test screen

--- DemoApp/app/screens/TestScreen.js
+++ DemoApp/app/screens/TestScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Button, ScrollView } from "react-native";
+import { View, Button, ScrollView, Image } from "react-native";
 
 function TestScreen() {
     return(
         <View style={{flex:1, backgroundColor: "plum", padding: 60}}>
             <ScrollView>
+                <Image source={{uri: "https://picsum.photos/300"}} style={{width: 300, height: 300}}/>
                 <Button
                     title="Press Test Button"
                     onPress={() => console.log("Test Button Pressed")}
