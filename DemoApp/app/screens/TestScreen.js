4dabff45fffa9dd828bcc14a9cd5c2f01c97ab81
Garrett <lamgarr@outlook.com>
2024-05-17 23:02:43 -0700

    Add Pressable to Image for Test screen

--- DemoApp/app/screens/TestScreen.js
+++ DemoApp/app/screens/TestScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, Button, ScrollView, Image } from "react-native";
+import { View, Button, ScrollView, Image, Pressable } from "react-native";
 
 function TestScreen() {
     return(
         <View style={{flex:1, backgroundColor: "plum", padding: 60}}>
             <ScrollView>
-                <Image source={{uri: "https://picsum.photos/300"}} style={{width: 300, height: 300}}/>
+                <Pressable onPress={() => console.log("image was pressed")}>
+                    <Image source={{uri: "https://picsum.photos/300"}} style={{width: 300, height: 300}}/>
+                </Pressable>
                 <Button
                     title="Press Test Button"
                     onPress={() => console.log("Test Button Pressed")}
