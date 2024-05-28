83e02b802963bc29ff8895448903ecb58de23327
Garrett <lamgarr@outlook.com>
2024-05-27 20:42:18 -0700

    Add ActivityIndicator to Test screen

--- DemoApp/app/screens/TestScreen.js
+++ DemoApp/app/screens/TestScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { View, Button, ScrollView, Image, Pressable, Text, Modal, StatusBar } from "react-native";
+import { View, Button, ScrollView, Image, Pressable, Text, Modal, StatusBar, ActivityIndicator } from "react-native";
 
 function TestScreen() {
 
@@ -9,6 +9,7 @@ function TestScreen() {
     return(
         <View style={{flex:1, backgroundColor: "plum", padding: 60}}>
             <StatusBar backgroundColor="white" barStyle="dark-content" hidden={false}/>
+            <ActivityIndicator size="medium" color="black" animating={true}/>
             <ScrollView>
                 <Pressable onPress={() => console.log("image was pressed")}>
                     <Image source={{uri: "https://picsum.photos/300"}} style={{width: 300, height: 300}}/>
