7d4452778bdd354dedac65a5065e519fa11c4afa
Garrett <lamgarr@outlook.com>
2024-05-30 21:52:46 -0700

    Add ActivityIndicator to Login screen

--- DemoApp/app/screens/LoginScreen.js
+++ DemoApp/app/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, Text, Button, Image, Pressable } from "react-native";
+import { SafeAreaView, Text, Button, Image, Pressable, ActivityIndicator } from "react-native";
 
 function LoginScreen({ route, navigation }) {
 
@@ -7,6 +7,7 @@ function LoginScreen({ route, navigation }) {
 
     return(
         <SafeAreaView style={styles.background}>
+            <ActivityIndicator size="medium" color="black" animating={true}/>
             <Text style={styles.text}>Log In, {name}</Text>
             <Pressable onPress={() => console.log("Login image pressed")}>
                 <Image source={{uri: "https://picsum.photos/300"}} style={contentImage}/>
