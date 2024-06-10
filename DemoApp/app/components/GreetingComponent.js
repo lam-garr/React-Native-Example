e2be753cd50e271d8e2592b4872582a933d869c2
Garrett <lamgarr@outlook.com>
2024-06-09 20:33:03 -0700

    Add StyleSheet to Greeting component

--- DemoApp/app/components/GreetingComponent.js
+++ DemoApp/app/components/GreetingComponent.js
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function GreetComponent({ name }) {
     return(
-        <View>
-            <Text>Greetings, {name}</Text>
+        <View style={styles.greetingContainer}>
+            <Text style={styles.greetingText}>Greetings, {name}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    greetingContainer: {
+        flex: 1,
+        backgroundColor: "plum",
+        padding: 60
+    },
+    greetingText: {
+        color: black
+    }
+})
