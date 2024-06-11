4597c9375101ddd3e2e2113464947eb47529afe3
Garrett <lamgarr@outlook.com>
2024-06-10 21:58:16 -0700

    Add StyleSheet to Test screen

--- DemoApp/app/screens/TestScreen.js
+++ DemoApp/app/screens/TestScreen.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { useState } from "react";
-import { View, Button, ScrollView, Image, Pressable, Text, Modal, StatusBar, ActivityIndicator, Alert } from "react-native";
+import { View, Button, ScrollView, Image, Pressable, Text, Modal, StatusBar, ActivityIndicator, Alert, StyleSheet } from "react-native";
 
 function TestScreen() {
 
     const [ modalIsVisible, setModalIsVisible ] = useState(false);
 
     return(
-        <View style={{flex:1, backgroundColor: "plum", padding: 60}}>
+        <View style={styles.testContainer}>
             <StatusBar backgroundColor="white" barStyle="dark-content" hidden={false}/>
             <ActivityIndicator size="medium" color="black" animating={true}/>
             <ScrollView>
                 <Pressable onPress={() => console.log("image was pressed")}>
-                    <Image source={{uri: "https://picsum.photos/300"}} style={{width: 300, height: 300}}/>
+                    <Image source={{uri: "https://picsum.photos/300"}} style={styles.testImg}/>
                 </Pressable>
                 <Button
                     title="alert"
@@ -40,7 +40,7 @@ function TestScreen() {
                     animationType="slide"
                     presentationStyle="pageSheet"
                 >
-                    <View style={{flex:1, backgroundColor: "yellow", padding: 60}}>
+                    <View style={styles.testModal}>
                         <Text>Modal Content</Text>
                         <Button
                             title="Close Modal"
@@ -53,5 +53,11 @@ function TestScreen() {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    testContainer:{flex:1, backgroundColor: "plum", padding: 60},
+    testModal:{flex:1, backgroundColor: "yellow", padding: 60},
+    testImg:{width: 300, height: 300}
+})
 
 export default TestScreen; 
