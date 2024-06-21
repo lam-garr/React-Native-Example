6e921afab47a451b1e077b330c12b0657282ee49
Garrett <lamgarr@outlook.com>
2024-06-20 22:40:17 -0700

    Add screen to demonstrate flex styling

---
+++ DemoApp/app/screens/FlexScreen.js
@@ -0,0 +1,21 @@
+import { StyleSheet, View } from "react-native";
+import BoxComponent from "../components/BoxComponent";
+
+export default function FlexScreen() {
+    return(
+        <View style={styles.container}>
+            <BoxComponent style={{ backgroundColor: "pink", flex: 1}}>Box 1</BoxComponent>
+            <BoxComponent style={{ backgroundColor: "blue", flex: 3}}>Box 2</BoxComponent>
+            <BoxComponent style={{ backgroundColor: "yellow"}}>Box 3</BoxComponent>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 64,
+        borderWidth: 6,
+        borderColor: "red"
+    }
+})
