108b005c46464916cff323d8e193d45ec7abca88
Garrett <lamgarr@outlook.com>
2024-06-21 22:27:07 -0700

    Add demonstration of flex direction styling

--- DemoApp/app/screens/FlexScreen.js
+++ DemoApp/app/screens/FlexScreen.js
@@ -14,6 +14,7 @@ export default function FlexScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: "column",
         marginTop: 64,
         borderWidth: 6,
         borderColor: "red"
