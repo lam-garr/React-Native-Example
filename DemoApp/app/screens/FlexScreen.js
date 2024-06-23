083963f672da5c4c54c3268a08c9af078263cbdd
Garrett <lamgarr@outlook.com>
2024-06-22 23:19:26 -0700

    Add demonstration of justify content styling

--- DemoApp/app/screens/FlexScreen.js
+++ DemoApp/app/screens/FlexScreen.js
@@ -6,7 +6,7 @@ export default function FlexScreen() {
         <View style={styles.container}>
             <BoxComponent style={{ backgroundColor: "pink", flex: 1}}>Box 1</BoxComponent>
             <BoxComponent style={{ backgroundColor: "blue", flex: 3}}>Box 2</BoxComponent>
-            <BoxComponent style={{ backgroundColor: "yellow"}}>Box 3</BoxComponent>
+            <BoxComponent style={{ backgroundColor: "yellow", flex: 1}}>Box 3</BoxComponent>
         </View>
     )
 }
@@ -15,6 +15,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "column",
+        justifyContent: "flex-start",
         marginTop: 64,
         borderWidth: 6,
         borderColor: "red"
