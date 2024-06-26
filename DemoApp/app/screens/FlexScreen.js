6806848d47a82e2701b7b4330b360f4f912d3ffb
Garrett <lamgarr@outlook.com>
2024-06-25 20:44:47 -0700

    Add demonstration for flex wrap styling

--- DemoApp/app/screens/FlexScreen.js
+++ DemoApp/app/screens/FlexScreen.js
@@ -17,6 +17,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "column",
+        flexWrap: "nowrap",
         justifyContent: "flex-start",
         alignItems: "stretch",
         marginTop: 64,
