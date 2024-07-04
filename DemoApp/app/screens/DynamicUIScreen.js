11f67418e03d2bcf1eb253135206ba7a19378fd3
Garrett <lamgarr@outlook.com>
2024-07-03 21:59:00 -0700

    Add demonstration of Dimensions API

--- DemoApp/app/screens/DynamicUIScreen.js
+++ DemoApp/app/screens/DynamicUIScreen.js
@@ -1,8 +1,17 @@
-import { StyleSheet, View }  from "react-native";
+import { StyleSheet, View, Text, Dimensions }  from "react-native";
 
 export default function DynamicUIScreen() {
-    return <View style={styles.container}></View>
+    return (
+        <View style={styles.container}>
+            <View style={styles.box}>
+                <Text style={styles.text}>Hello!</Text>
+            </View>
+        </View>
+    )
 }
+
+const windoWidth = Dimensions.get("window").width;
+const windoHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
     container: {
@@ -10,5 +19,15 @@ const styles = StyleSheet.create({
         backgroundColor: "plum",
         alignItems: "center",
         justifyContent: "center"
+    },
+    box: {
+        width: windowWidth > 500 ? "70%" : "90%",
+        height: windowHeight > 600 ? "60%" : "90%",
+        backgroundColor: "lightBlue",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    text: {
+        fontSize: windowWidth > 500 ? 50 : 24
     }
 })
