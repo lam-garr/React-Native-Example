e29bae33a806d65e33bf24dd008a26bd54c46502
Garrett <lamgarr@outlook.com>
2024-07-04 21:26:22 -0700

    Add demonstration of useWindowDimensions

--- DemoApp/app/screens/DynamicUIScreen.js
+++ DemoApp/app/screens/DynamicUIScreen.js
@@ -1,17 +1,24 @@
-import { StyleSheet, View, Text, Dimensions }  from "react-native";
+import { StyleSheet, View, Text, useWindowDimensions }  from "react-native";
 
 export default function DynamicUIScreen() {
+
+    const windowWidth = useWindowDimensions.get("window").width;
+    const windowHeight = useWindowDimensions.get("window").height;
+
     return (
         <View style={styles.container}>
-            <View style={styles.box}>
-                <Text style={styles.text}>Hello!</Text>
+            <View style={[
+                styles.box, 
+                {
+                    width: windowWidth > 500 ? "70%" : "90%",
+                    height: windowHeight > 600 ? "60%" : "90%"
+                }
+                ]}>
+                <Text style={{fontSize: windowWidth > 500 ? 50 : 24}}>Hello!</Text>
             </View>
         </View>
     )
 }
-
-const windoWidth = Dimensions.get("window").width;
-const windoHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
     container: {
@@ -21,13 +28,8 @@ const styles = StyleSheet.create({
         justifyContent: "center"
     },
     box: {
-        width: windowWidth > 500 ? "70%" : "90%",
-        height: windowHeight > 600 ? "60%" : "90%",
         backgroundColor: "lightBlue",
         alignItems: "center",
         justifyContent: "center"
-    },
-    text: {
-        fontSize: windowWidth > 500 ? 50 : 24
     }
 })
