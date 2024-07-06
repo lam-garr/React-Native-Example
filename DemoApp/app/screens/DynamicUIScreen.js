9aca94c5e284f9a49ea70fb4e4027565df05e442
Garrett <lamgarr@outlook.com>
2024-07-05 22:04:18 -0700

    Add demonstration of SafeAreaView

--- DemoApp/app/screens/DynamicUIScreen.js
+++ DemoApp/app/screens/DynamicUIScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, useWindowDimensions }  from "react-native";
+import { StyleSheet, View, Text, SafeAreaView}  from "react-native";
 
 export default function DynamicUIScreen() {
 
@@ -6,21 +6,27 @@ export default function DynamicUIScreen() {
     const windowHeight = useWindowDimensions.get("window").height;
 
     return (
-        <View style={styles.container}>
-            <View style={[
-                styles.box, 
-                {
-                    width: windowWidth > 500 ? "70%" : "90%",
-                    height: windowHeight > 600 ? "60%" : "90%"
-                }
-                ]}>
-                <Text style={{fontSize: windowWidth > 500 ? 50 : 24}}>Hello!</Text>
+        <SafeAreaView style={styles.safeContainer}>
+            <View style={styles.container}>
+                <View style={[
+                    styles.box, 
+                    {
+                        width: windowWidth > 500 ? "70%" : "90%",
+                        height: windowHeight > 600 ? "60%" : "90%"
+                    }
+                    ]}>
+                    <Text style={{fontSize: windowWidth > 500 ? 50 : 24}}>Hello!</Text>
+                </View>
             </View>
-        </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    safeContainer: {
+        flex: 1,
+        backgroundColor: "plum"
+    },
     container: {
         flex: 1,
         backgroundColor: "plum",
