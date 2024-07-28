20a62a5c7071417dc219a130efcb41213c781650
Garrett <lamgarr@outlook.com>
2024-07-27 22:52:24 -0700

    Add LoginForm screen

---
+++ DemoApp/app/screens/LoginFormScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { useState} from "react";
+
+export default function LoginFormScreen() {
+    return(
+        <View></View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
