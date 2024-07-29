87a9f265959c09f71209163856ef7e5b5adf7e61
Garrett <lamgarr@outlook.com>
2024-07-28 22:35:23 -0700

    Add styling to LoginForm screen

--- DemoApp/app/screens/LoginFormScreen.js
+++ DemoApp/app/screens/LoginFormScreen.js
@@ -3,10 +3,32 @@ import { useState} from "react";
 
 export default function LoginFormScreen() {
     return(
-        <View></View>
+        <View style={styles.container}>
+            <View style={styles.form}>
+
+            </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal: 20,
+        backgroundColor: "#f5f5f5"
+    },
+    form: {
+        backgroundColor: "white",
+        padding: 20,
+        borderRadius: 10,
+        shadowColor: "black",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    }
 })
