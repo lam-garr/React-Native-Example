2988e5a733273a1b560d4bcc91ecdfd4793dd871
Garrett <lamgarr@outlook.com>
2024-08-06 22:35:23 -0700

    Add GET request to retrieve sample data

--- DemoApp/app/screens/DemoNetworkScreen.js
+++ DemoApp/app/screens/DemoNetworkScreen.js
@@ -1,6 +1,12 @@
 import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 
 export default function DemoNetworkScreen() {
+
+    const fetchData = async () => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`);
+        const data = await response.json();
+    }
+
     return(
         <SafeAreaView style={styles.container}>
         </SafeAreaView>
