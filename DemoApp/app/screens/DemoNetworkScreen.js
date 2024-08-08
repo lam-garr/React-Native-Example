99bd365117c6265485cea8c03118c520b9379499
Garrett <lamgarr@outlook.com>
2024-08-07 22:54:22 -0700

    Add state to store retreived data from GET request

--- DemoApp/app/screens/DemoNetworkScreen.js
+++ DemoApp/app/screens/DemoNetworkScreen.js
@@ -1,10 +1,14 @@
 import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import { useState } from "react";
 
 export default function DemoNetworkScreen() {
 
-    const fetchData = async () => {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`);
+    const [ dataList, setDataList ] = useState([])
+
+    const fetchData = async (limit = 10) => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
         const data = await response.json();
+        setDataList(data);
     }
 
     return(
