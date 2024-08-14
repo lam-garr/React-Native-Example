9c1c281ea272e1d94f7897ffd08d49f80e10988b
Garrett <lamgarr@outlook.com>
2024-08-13 22:03:14 -0700

    Add handling for loading state

--- DemoApp/app/screens/DemoNetworkScreen.js
+++ DemoApp/app/screens/DemoNetworkScreen.js
@@ -1,19 +1,30 @@
-import { StyleSheet, SafeAreaView, StatusBar, View, FlatList, Text } from "react-native";
+import { StyleSheet, SafeAreaView, StatusBar, View, FlatList, Text, ActivityIndicator } from "react-native";
 import { useState, useEffect } from "react";
 
 export default function DemoNetworkScreen() {
 
     const [ dataList, setDataList ] = useState([])
+    const [ isLoading, setIsLoading ] = useState(true);
 
     const fetchData = async (limit = 10) => {
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
         const data = await response.json();
         setDataList(data);
+        setIsLoading(false);
     }
 
     useEffect(() => {
         fetchData();
     }, [])
+
+    if(isLoading) {
+        return(
+            <SafeAreaView>
+                <ActivityIndicator/>
+                <Text>Loading...</Text>
+            </SafeAreaView>
+        )
+    }
 
     return(
         <SafeAreaView style={styles.container}>
