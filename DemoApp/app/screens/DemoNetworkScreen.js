d63a24079c84ef13ba8f9b05166cd0fecda94b4f
Garrett <lamgarr@outlook.com>
2024-08-16 23:15:04 -0700

    Add state to handle refreshing

--- DemoApp/app/screens/DemoNetworkScreen.js
+++ DemoApp/app/screens/DemoNetworkScreen.js
@@ -5,6 +5,7 @@ export default function DemoNetworkScreen() {
 
     const [ dataList, setDataList ] = useState([])
     const [ isLoading, setIsLoading ] = useState(true);
+    const [ refreshing, setRefreshing ] = useState(false);
 
     const fetchData = async (limit = 10) => {
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
