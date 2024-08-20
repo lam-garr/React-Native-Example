a61755b5b54318986cea51b69349924072ed5010
Garrett <lamgarr@outlook.com>
2024-08-19 22:45:45 -0700

    Add state to track POST request

--- DemoApp/app/screens/DemoNetworkScreen.js
+++ DemoApp/app/screens/DemoNetworkScreen.js
@@ -6,6 +6,7 @@ export default function DemoNetworkScreen() {
     const [ dataList, setDataList ] = useState([])
     const [ isLoading, setIsLoading ] = useState(true);
     const [ refreshing, setRefreshing ] = useState(false);
+    const [ isPosting, setIsPosting ] = useState(false);
 
     const fetchData = async (limit = 10) => {
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
