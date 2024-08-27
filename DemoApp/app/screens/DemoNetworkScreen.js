56fa1097dfa273133754a1affb3660dc95455225
Garrett <lamgarr@outlook.com>
2024-08-26 23:19:32 -0700

    Add state to handle errors

--- DemoApp/app/screens/DemoNetworkScreen.js
+++ DemoApp/app/screens/DemoNetworkScreen.js
@@ -9,6 +9,7 @@ export default function DemoNetworkScreen() {
     const [ postTitle, setPostTitle ] = useState("");
     const [ postBody, setPostBody ] = useState("");
     const [ isPosting, setIsPosting ] = useState(false);
+    const [ errors, setErrors ] = useState("");
 
     const fetchData = async (limit = 10) => {
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
