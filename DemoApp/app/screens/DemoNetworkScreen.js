8ab48e8d7b0d91c1133d686b449b4a7fd044b8a3
Garrett <lamgarr@outlook.com>
2024-08-29 22:55:32 -0700

    Add try catch for fetching data

--- DemoApp/app/screens/DemoNetworkScreen.js
+++ DemoApp/app/screens/DemoNetworkScreen.js
@@ -12,10 +12,17 @@ export default function DemoNetworkScreen() {
     const [ errors, setErrors ] = useState("");
 
     const fetchData = async (limit = 10) => {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
-        const data = await response.json();
-        setDataList(data);
-        setIsLoading(false);
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
+            const data = await response.json();
+            setDataList(data);
+            setIsLoading(false);
+            setErrors("");
+        } catch (error) {
+            console.log("error fetching data", error);
+            setIsLoading(false);
+            setErrors("Failed to fetch data list.")
+        }
     }
 
     const handleRefresh = () => {
