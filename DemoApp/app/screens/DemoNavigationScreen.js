a1020b94a2e501db5864caccf889f64aac75fafe
Garrett <lamgarr@outlook.com>
2024-09-03 22:07:05 -0700

    Add createNativeStackNavigator

--- DemoApp/app/screens/DemoNavigationScreen.js
+++ DemoApp/app/screens/DemoNavigationScreen.js
@@ -1,4 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator;
 
 export default function DemoNavigationScreen() {
     return(
