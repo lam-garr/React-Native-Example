bbc0117517f80c39314a0c12dedb57964f628b82
Garrett <lamgarr@outlook.com>
2024-09-06 23:12:05 -0700

    Add HomeStack screen to Stack Navigator

--- DemoApp/app/screens/DemoNavigationScreen.js
+++ DemoApp/app/screens/DemoNavigationScreen.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeStackScreen from ".HomeStackScreen";
 
 const Stack = createNativeStackNavigator;
 
@@ -7,7 +8,7 @@ export default function DemoNavigationScreen() {
     return(
         <NavigationContainer>
             <Stack.Navigator>
-                {/* <Stack.Screen/> */}
+                <Stack.Screen name="Home" component={HomeStackScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
