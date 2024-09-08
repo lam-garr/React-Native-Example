02a578b355d9db12777dd9559f71db68ca95e0dc
Garrett <lamgarr@outlook.com>
2024-09-07 23:14:36 -0700

    Add initialRouteName to Stack Navigator

--- DemoApp/app/screens/DemoNavigationScreen.js
+++ DemoApp/app/screens/DemoNavigationScreen.js
@@ -7,7 +7,7 @@ const Stack = createNativeStackNavigator;
 export default function DemoNavigationScreen() {
     return(
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeStackScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
