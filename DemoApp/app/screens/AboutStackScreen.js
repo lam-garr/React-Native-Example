aa7c50180a78e006b707a7ff9b68c86b07bce56d
Garrett <lamgarr@outlook.com>
2024-09-14 23:40:07 -0700

    Add handling to navigate to Home from About

--- DemoApp/app/screens/AboutStackScreen.js
+++ DemoApp/app/screens/AboutStackScreen.js
@@ -1,10 +1,10 @@
 import { View, Text, StyleSheet } from "react-native";
 
-export default function AboutStackScreen() {
+export default function AboutStackScreen({ navigation }) {
     return(
         <View style={styles.container}>
             <Text style={styles.text}>About Screen</Text>
-            <Button title="Go to Home" onPress={() => {}}/>
+            <Button title="Go to Home" onPress={() => navigation.navigate("Home")}/>
         </View>
     )
 }
