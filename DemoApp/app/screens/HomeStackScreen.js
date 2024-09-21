cf12be80949b338c7c0f8a407c02cc420e06bd7f
Garrett <lamgarr@outlook.com>
2024-09-20 22:49:09 -0700

    Add handling in render data from params

--- DemoApp/app/screens/HomeStackScreen.js
+++ DemoApp/app/screens/HomeStackScreen.js
@@ -1,11 +1,12 @@
 import { View, Text, StyleSheet, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-export default function HomeStackScreen() {
+export default function HomeStackScreen({ route }) {
     const navigation = useNavigation();
     return(
         <View style={styles.container}>
             <Text style={styles.text}>Home Screen</Text>
+            <Text style={style.text}>{route.params?.result}</Text>
             <Button title="Go to About" onPress={() => navigation.navigate("About", {name: "User"})}/>
         </View>
     )
