c9f2db619446a28dea5b1a09b5d011078fd0f715
Garrett <lamgarr@outlook.com>
2024-10-04 23:15:59 -0700

    Add DemoDrawerNav screen

---
+++ DemoApp/app/screens/DemoDrawerNavScreen.js
@@ -0,0 +1,8 @@
+import "react-navigation-gesture-handler";
+import { createDrawerNavigation } from "@react-navigation/drawer";
+
+const Drawer = createDrawerNavigation();
+
+export default function DemoDrawerNavScreen(){
+    return()
+}
