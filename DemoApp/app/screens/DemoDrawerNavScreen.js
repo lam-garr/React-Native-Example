0fd07216c8fdbf99155fcf8995ce08853d8f1962
Garrett <lamgarr@outlook.com>
2024-10-05 23:28:20 -0700

    Add Drawer Navigator

--- DemoApp/app/screens/DemoDrawerNavScreen.js
+++ DemoApp/app/screens/DemoDrawerNavScreen.js
@@ -1,8 +1,15 @@
 import "react-navigation-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigation } from "@react-navigation/drawer";
 
 const Drawer = createDrawerNavigation();
 
 export default function DemoDrawerNavScreen(){
-    return()
+    return(
+        <NavigationContainer>
+            <Drawer.Navigator>
+                <Drawer.Screen/>
+            </Drawer.Navigator>
+        </NavigationContainer>
+    )
 }
