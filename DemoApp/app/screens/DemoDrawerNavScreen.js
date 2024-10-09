cf83c3a70a70f02232b52c5d0a178dbf8401212a
Garrett <lamgarr@outlook.com>
2024-10-08 20:58:30 -0700

    Add Dash screen to Drawer Navigator

--- DemoApp/app/screens/DemoDrawerNavScreen.js
+++ DemoApp/app/screens/DemoDrawerNavScreen.js
@@ -1,6 +1,7 @@
 import "react-navigation-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigation } from "@react-navigation/drawer";
+import DemoDashScreen from "./DemoDashScreen";
 
 const Drawer = createDrawerNavigation();
 
@@ -8,7 +9,7 @@ export default function DemoDrawerNavScreen(){
     return(
         <NavigationContainer>
             <Drawer.Navigator>
-                <Drawer.Screen/>
+                <Drawer.Screen name="Dashboard" component={DemoDashScreen}/>
             </Drawer.Navigator>
         </NavigationContainer>
     )
