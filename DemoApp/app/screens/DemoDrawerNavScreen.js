6e7b71e34e2d609c8d308d66668986eb55c634c3
Garrett <lamgarr@outlook.com>
2024-10-20 20:47:50 -0700

    Add drawerActiveBackgroundColor option

--- DemoApp/app/screens/DemoDrawerNavScreen.js
+++ DemoApp/app/screens/DemoDrawerNavScreen.js
@@ -30,6 +30,7 @@ export default function DemoDrawerNavScreen(){
                         title: "Settings",
                         drawerLabel: "Settings",
                         drawerActiveTintColor: "#333",
+                        drawerActiveBackgroundColor: "lightblue"
                     }}
                 />
             </Drawer.Navigator>
