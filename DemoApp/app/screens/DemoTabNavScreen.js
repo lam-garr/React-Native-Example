68788e7585c47efcfbb45fee87e501bd7ef83ff9
Garrett <lamgarr@outlook.com>
2024-10-23 22:47:06 -0700

    Add DemoTabNav screen

---
+++ DemoApp/app/screens/DemoTabNavScreen.js
@@ -0,0 +1,8 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+export default function DemoTabNavScreen() {
+    return();
+}
