90f9b5dbdc19affa4b247d42311961c180b4995b
Garrett <lamgarr@outlook.com>
2024-10-27 20:29:35 -0700

    Add Profile screen to Tab Navigator

--- DemoApp/app/screens/DemoTabNavScreen.js
+++ DemoApp/app/screens/DemoTabNavScreen.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DemoSettingScreen from "./DemoSettingScreen";
+import DemoProfileScreen from "./DemoProfileScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -8,6 +9,7 @@ export default function DemoTabNavScreen() {
     return(
         <NavigationContainer>
             <Tab.Navigator>
+                <Tab.Screen name="Profile" component={DemoProfileScreen}/>
                 <Tab.Screen name="Settings" component={DemoSettingScreen}/>
             </Tab.Navigator>
         </NavigationContainer>
