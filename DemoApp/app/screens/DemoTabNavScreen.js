66f02e91eed6ce56a51a89366a49919772827dcd
Garrett <lamgarr@outlook.com>
2024-10-29 18:16:36 -0700

    Add Catalog screen to Tab Navigator

--- DemoApp/app/screens/DemoTabNavScreen.js
+++ DemoApp/app/screens/DemoTabNavScreen.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DemoSettingScreen from "./DemoSettingScreen";
 import DemoProfileScreen from "./DemoProfileScreen";
+import DemoCatalogList from "./DemoCatalogListScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,6 +11,7 @@ export default function DemoTabNavScreen() {
         <NavigationContainer>
             <Tab.Navigator>
                 <Tab.Screen name="Profile" component={DemoProfileScreen}/>
+                <Tab.Screen name="Catalog" component={DemoCatalogList}/>
                 <Tab.Screen name="Settings" component={DemoSettingScreen}/>
             </Tab.Navigator>
         </NavigationContainer>
